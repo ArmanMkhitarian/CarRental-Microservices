db965533ed6f4e7e42d7e46fcdceacc4cb99daa7
AMkhitaryan <AMkhitaryan@ibs.ru>
2025-03-06 14:46:42 +0300

    Commit

--- services/CarBookingService/Kafka/BookingProducer.cs
+++ services/CarBookingService/Kafka/BookingProducer.cs
@@ -12,7 +12,7 @@ namespace CarBookingService.Kafka
         {
             var config = new ProducerConfig
             {
-                BootstrapServers = "localhost:9092"
+                BootstrapServers = "kafka:9092"
             };
 
             _producer = new ProducerBuilder<Null, string>(config).Build();
