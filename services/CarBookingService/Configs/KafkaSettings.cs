6db8fcf2a30aec03b9dc9166600e68cc1522abf2
AMkhitaryan <AMkhitaryan@ibs.ru>
2025-03-06 16:01:05 +0300

    Add

---
+++ services/CarBookingService/Configs/KafkaSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CarBookingService.Configs
+{
+    public class KafkaSettings
+    {
+        public string BootstrapServers { get; set; } = "localhost:9092";
+        public string Topic { get; set; } = "car-bookings";
+    }
+}
