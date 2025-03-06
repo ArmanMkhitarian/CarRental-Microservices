96a843716cd33ba36ced2faf44c69570cac20cdc
AMkhitaryan <AMkhitaryan@ibs.ru>
2025-03-06 15:07:28 +0300

    add logging

--- services/CarBookingService/Program.cs
+++ services/CarBookingService/Program.cs
@@ -1,8 +1,14 @@
 using CarBookingService.Configs;
 using CarBookingService.Kafka;
 using CarBookingService.Services;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()  // Логирование в консоль
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 // Add services to the container.
 builder.Services.AddSingleton<BookingProducer>();
